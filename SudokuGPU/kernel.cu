9e2edbb83a97bbc96996e4c2f7191c175f9fca28
StiffGitt <131384247+StiffGitt@users.noreply.github.com>
2023-12-06 19:44:42 +0100

    dzialabfs

--- SudokuGPU/kernel.cu
+++ SudokuGPU/kernel.cu
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <iostream>
 
+#define BOARD_FILE "board.txt"
 #define N 9
 #define R 81
 #define C 90
@@ -14,7 +15,7 @@
 typedef unsigned int Uint;
 #define CLEAR_BIT(a, b) a &= ~((Uint)1 << b);
 #define CHECK_BIT(a, b) (bool)((a >> b) & (Uint)1)
-#define GET_ROW(i) (int)(R + i / N);
+#define GET_ROW(i) (R + i / N);
 #define GET_COLUMN(i) (int)(C + i % N);
 #define GET_SUBBOARD(i) (int)(S + ((i / N) / 3) * 3 + ((i % N) / 3));
 
@@ -24,6 +25,7 @@ Uint* initializeBoard(Uint* board);
 void initializeRows(Uint* board);
 void initializeColumns(Uint* board);
 void initializeSubboards(Uint* board);
+Uint* loadBoard();
 
 __global__ void cudaBFS(Uint* oldBoard, Uint* newBoard, int boardsCount, int *lastBoard)
 {
@@ -42,12 +44,12 @@ __global__ void cudaBFS(Uint* oldBoard, Uint* newBoard, int boardsCount, int *la
         if (i == boardBegin + N * N)
             return;
 
-        int r = boardBegin + GET_ROW(i - boardBegin);
-        int c = boardBegin + GET_COLUMN(i - boardBegin);
-        int s = boardBegin + GET_SUBBOARD(i - boardBegin);
+        int r = boardBegin + GET_ROW((i - boardBegin));
+        int c = boardBegin + GET_COLUMN((i - boardBegin));
+        int s = boardBegin + GET_SUBBOARD((i - boardBegin));
         for (int number = 1; number <= N; number++)
         {
-            if (CHECK_BIT(oldBoard[r], number - 1) && CHECK_BIT(oldBoard[c], number - 1) && CHECK_BIT(oldBoard[s], number - 1))
+            if (CHECK_BIT(oldBoard[r], (number - 1)) && CHECK_BIT(oldBoard[c], (number - 1)) && CHECK_BIT(oldBoard[s], (number - 1)))
             {
                 int curBoard = atomicAdd(lastBoard, 1);
                 int newBegin = curBoard * BSIZE;
@@ -56,9 +58,9 @@ __global__ void cudaBFS(Uint* oldBoard, Uint* newBoard, int boardsCount, int *la
                     newBoard[newBegin + j] = oldBoard[boardBegin + j];
                 }
                 newBoard[newBegin + i - boardBegin] = number;
-                CLEAR_BIT(newBoard[newBegin + r - boardBegin], number - 1);
-                CLEAR_BIT(newBoard[newBegin + c - boardBegin], number - 1);
-                CLEAR_BIT(newBoard[newBegin + s - boardBegin], number - 1);
+                CLEAR_BIT(newBoard[newBegin + r - boardBegin], (number - 1));
+                CLEAR_BIT(newBoard[newBegin + c - boardBegin], (number - 1));
+                CLEAR_BIT(newBoard[newBegin + s - boardBegin], (number - 1));
             }
         }
         idx += blockDim.x * gridDim.x;
@@ -67,7 +69,7 @@ __global__ void cudaBFS(Uint* oldBoard, Uint* newBoard, int boardsCount, int *la
 
 int main()
 {
-    Uint board[]{
+    /*Uint board[]{
         0, 4, 0, 0, 0, 0, 1, 7, 9,
         0, 0, 2, 0, 0, 8, 0, 5, 4,
         0, 0, 6, 0, 0, 5, 0, 0, 8,
@@ -76,8 +78,8 @@ int main()
         0, 1, 9, 0, 6, 0, 0, 4, 0,
         3, 0, 0, 4, 0, 0, 7, 0, 0,
         5, 7, 0, 1, 0, 0, 2, 0, 0,
-        9, 2, 8, 0, 0, 0, 0, 6, 0 };
-    //printBoard(board);
+        9, 2, 8, 0, 0, 0, 0, 6, 0 };*/
+    Uint* board = loadBoard();
     solveSudoku(board);
     return 0;
 }
@@ -97,27 +99,26 @@ Uint* solveSudoku(Uint *inBoard)
 
     cudaMemset(board1, 0, boardMem * sizeof(Uint));
     cudaMemset(board2, 0, boardMem * sizeof(Uint));
-    cudaMemset(lastBoard, 0, sizeof(int));
 
     cudaMemcpy(board1, board, BSIZE * sizeof(Uint), cudaMemcpyHostToDevice);
     int boardsCount = 1;
     
     for (int it = 0; it < ITERATIONS; it++)
     {
+        cudaMemset(lastBoard, 0, sizeof(int));
         //sie pierdoli
         if(it % 2 == 0)
             cudaBFS<<< BLOCKS, THREADS >>>(board1, board2, boardsCount, lastBoard);
         else
-            cudaBFS <<<BLOCKS, THREADS >>> (board2, board1, boardsCount, lastBoard);
+            cudaBFS<<< BLOCKS, THREADS >>> (board2, board1, boardsCount, lastBoard);
 
         cudaMemcpy(&boardsCount, lastBoard, sizeof(int), cudaMemcpyDeviceToHost);
-
-        printf("total boards after an iteration %d: %d\n", it, boardsCount);
-
-        cudaMemset(lastBoard, 0, sizeof(int));
+        
+         printf("total boards after an iteration %d: %d\n", it, boardsCount);
     }
 
-    cudaMemcpy(board, board1, BSIZE * sizeof(Uint), cudaMemcpyDeviceToHost);
+    Uint* bfsBoard = (ITERATIONS % 2 == 0)? board1 : board2;
+    cudaMemcpy(board, bfsBoard, BSIZE * sizeof(Uint), cudaMemcpyDeviceToHost);
     printBoard(board);
 
     cudaFree(board1);
@@ -176,7 +177,7 @@ void initializeRows(Uint* board)
         for (int i = 0; i < N; i++)
         {
             if (board[r * N + i] > 0)
-                CLEAR_BIT(board[R + r], board[r * N + i] - 1);
+                CLEAR_BIT(board[R + r], (board[r * N + i] - 1));
         }
     }
 }
@@ -189,7 +190,7 @@ void initializeColumns(Uint* board)
         for (int i = 0; i < N; i++)
         {
             if (board[i * N + c] > 0)
-                CLEAR_BIT(board[C + c], board[i * N + c] - 1);
+                CLEAR_BIT(board[C + c], (board[i * N + c] - 1));
         }
     }
 }
@@ -205,7 +206,40 @@ void initializeSubboards(Uint* board)
             int r = i / 3;
             int c = i % 3;
             if (board[start + r * N + c] > 0)
-                CLEAR_BIT(board[S + s], board[start + r * N + c] - 1);
+                CLEAR_BIT(board[S + s], (board[start + r * N + c] - 1));
         }
     }
+}
+
+Uint* loadBoard()
+{
+    FILE* boardFile = fopen(BOARD_FILE, "r");
+    Uint* board = (Uint*)malloc(N * N * sizeof(Uint));
+    char c;
+
+    if (boardFile == NULL)
+    {
+        printf("boardFile error\n");
+        return board;
+    }
+
+    for (int i = 0; i < N; i++) 
+    {
+        for (int j = 0; j < N; j++) 
+        {
+            if (!fscanf(boardFile, "%c\n", &c)) 
+            {
+                printf("boardFile error\n");
+                return board;
+            }
+
+            if (c >= '1' && c <= '9') {
+                board[i * N + j] = (Uint)(c - '0');
+            }
+            else {
+                board[i * N + j] = 0;
+            }
+        }
+    }
+    return board;
 }
